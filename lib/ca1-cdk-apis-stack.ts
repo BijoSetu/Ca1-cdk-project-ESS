243697229fafbbfbfeab84aa22be5414d8ade166
bijo-at-688165709689 <bijo@cordovacloud.com>
2025-03-17 15:10:40 +0000

    initial deploy , movie review table created and seeded with data

--- lib/ca1-cdk-apis-stack.ts
+++ lib/ca1-cdk-apis-stack.ts
@@ -1,16 +1,48 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import { generateBatch } from "../shared/utils";
+import * as custom from "aws-cdk-lib/custom-resources";
+import {movieReviews} from "../seed/movieReviews"
+ 
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class Ca1CdkApisStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+   // Tables 
+   const moviesReviewsTable = new dynamodb.Table(this, "MoviesReviews", {
+    billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    partitionKey: { name: "MovieId", type: dynamodb.AttributeType.NUMBER },
+    sortKey: { name: "ReviewId", type: dynamodb.AttributeType.NUMBER },
+    removalPolicy: cdk.RemovalPolicy.DESTROY,
+    tableName: "MovieReviews",
+  });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'Ca1CdkApisQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+
+
+    // resources 
+    new custom.AwsCustomResource(this, "movieReviewsInitData", {
+      onCreate: {
+        service: "DynamoDB",
+        action: "batchWriteItem",
+        parameters: {
+          RequestItems: {
+            [moviesReviewsTable.tableName]: generateBatch(movieReviews),
+          },
+        },
+        physicalResourceId: custom.PhysicalResourceId.of("movieReviewsInitData"),
+      },
+      policy: custom.AwsCustomResourcePolicy.fromSdkCalls({
+        resources: [moviesReviewsTable.tableArn],
+      }),
+    });
   }
+
+ 
+ 
+ 
+
 }
---
+++ seed/movieReviews.ts
@@ -0,0 +1,40 @@
+import { MovieReview } from "../shared/types";
+
+export const movieReviews : MovieReview[]  = [
+    {
+      MovieId: 1,
+      ReviewId: 101,
+      ReviewerId: "john.doe@example.com",
+      ReviewDate: "2025-01-20",
+      Content: "A fantastic movie with stunning visuals and a great storyline.",
+    },
+    {
+      MovieId: 1,
+      ReviewId: 102,
+      ReviewerId: "jane.smith@example.com",
+      ReviewDate: "2025-01-21",
+      Content: "An average movie, good for a one-time watch.",
+    },
+    {
+      MovieId: 2,
+      ReviewId: 103,
+      ReviewerId: "mark.jones@example.com",
+      ReviewDate: "2025-02-10",
+      Content: "One of the best performances by the lead actor!",
+    },
+    {
+      MovieId: 3,
+      ReviewId: 104,
+      ReviewerId: "lisa.brown@example.com",
+      ReviewDate: "2025-03-05",
+      Content: "The storyline was a bit weak, but the action scenes were great.",
+    },
+    {
+      MovieId: 2,
+      ReviewId: 105,
+      ReviewerId: "tom.wilson@example.com",
+      ReviewDate: "2025-02-15",
+      Content: "A must-watch! The direction and cinematography were top-notch.",
+    },
+  ];
+  
