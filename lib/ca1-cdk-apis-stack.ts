48de0890cd913a817deec13dc7e8ebe49726c401
bijo-at-688165709689 <bijo@cordovacloud.com>
2025-03-18 17:38:58 +0000

    added schema for authentication - signup - sign in - confirm sign up

--- lib/ca1-cdk-apis-stack.ts
+++ lib/ca1-cdk-apis-stack.ts
@@ -1,16 +1,38 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
 import {AppApiConstruct } from './app-api-construct'
-
+import { Construct } from "constructs";
+import { UserPool } from "aws-cdk-lib/aws-cognito";
+import {AuthApiConstruct} from './auth-api-construct'
+ 
 
 
 export class Ca1CdkApisStack extends cdk.Stack {
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+  const userPool = new UserPool(this, "UserPool", {
+    signInAliases: { username: true, email: true },
+    selfSignUpEnabled: true,
+    removalPolicy: cdk.RemovalPolicy.DESTROY,
+  });
+
+  const userPoolId = userPool.userPoolId;
+
+  const appClient = userPool.addClient("AppClient", {
+    authFlows: { userPassword: true },
+  });
+
+  const userPoolClientId = appClient.userPoolClientId;
+
+  new AuthApiConstruct(this, 'AuthApiConstruct', {
+    userPoolId: userPoolId,
+    userPoolClientId: userPoolClientId,
+  });
+
   new AppApiConstruct(this, 'AppApiConstruct');
   }
 }
-
 
 
 
